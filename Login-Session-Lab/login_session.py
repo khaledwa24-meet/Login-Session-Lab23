2df5ce49e106c2ee8aa802f95c4d0f37e26669c3
khaledwa24-meet <139680869+khaledwa24-meet@users.noreply.github.com>
2023-07-24 15:30:26 +0300

    done i will start with my other labs now since i understand about the login sessions more now

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,11 +4,22 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods = ['GET' , 'POST']) # What methods are needed?
 def home():
+	if request.method == "POST":
+		try:
+			age = request.form['age']
+			name = request.form['name']
+			quote = request.form['quote']
+			login_session['age'] = age
+			login_session['name'] = name
+			login_session['quote'] = quote
+			return render_template('thanks.html')
+		except:
+			return render_template('error.html')
+	else:
+		return render_template('home.html')
 	
-	return render_template('home.html')
-
 
 @app.route('/error')
 def error():
@@ -18,8 +29,8 @@ def error():
 
 @app.route('/display')
 def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	print(login_session)
+	return render_template('display.html', name = login_session['name'], age = login_session['age'], quote = login_session['quote']) # What variables are needed?
 
 
 @app.route('/thanks')
